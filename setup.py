757d75d4bd7e8083ed84425bb381a2082daf7d8c
Uros Trstenjak <uros.trstenjak@gmail.com>
2019-02-26 22:27:42 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as requirements:
+    REQUIREMENTS = requirements.readlines()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-funky-sheets',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='Django implementation of Handsontable spreadsheets for CRUD actions.',
+    long_description=README,
+    url='https://github.com/trco/django-funky-sheets',
+    author='Uros Trstenjak',
+    author_email='uros.trstenjak@gmail.com',
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
